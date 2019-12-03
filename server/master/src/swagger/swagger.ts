5173d6b37c83c93c13f7100acd03c2e972afc952
Khanh Duy <duy.nguyenkhanh.ncc@gmail.com>
2019-12-04 01:45:00 +0700

    init

---
+++ server/master/src/swagger/swagger.ts
@@ -0,0 +1,14 @@
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+const options = new DocumentBuilder()
+    .setTitle('Rotoya')
+    .setDescription('The Rotoya API description')
+    .setVersion('1.0')
+    .addTag('Rotoya')
+    .build();
+
+export function initSwagger(app: INestApplication) {
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+}
