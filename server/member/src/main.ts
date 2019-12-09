8ab0b8a194b762f7a9d9cffb6d25291b9034ef89
Khanh Duy <duy.nguyenkhanh.ncc@gmail.com>
2019-12-09 11:47:46 +0700

    init member

--- server/member/src/main.ts
+++ server/member/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstraps() {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-bootstraps();
+bootstrap();
